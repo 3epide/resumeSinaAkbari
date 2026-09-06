27bf4b92ed454fcee7ce1d0a7f493c0098331cc2
spd <spd.81.2002@gmail.com>
2026-09-07 01:47:55 +0330

    translation file add and fixed EN until expertise

---
+++ translations.js
@@ -0,0 +1,107 @@
+const translations = {
+    en: {
+        //       navbar    
+        nav: {
+            home: "Home",
+            about: "About",
+            expertise: "Expertise",
+            experience: "Experience",
+            projects: "Projects",
+            contact: "Contact",
+            getInTouch: "GET IN TOUCH"
+        },
+        //      hero      
+        hero: {
+            label: "MECHANICAL DESIGN ENGINEER",
+            titleStart: "I design",
+            titleHighLight: "mechanical systems",
+            titleEnd: "that turn ideas into reality.",
+            description: "Mechanical Design Engineer specializing in mechanical systems, CAD design, simulation,prototyping and product development.",
+            viewProjects: "VIEW PROJECTS",
+            aboutMe: "ABOUT ME",
+            yearsExperience: "Years Experience",
+            patents: " Patents",
+            engineeringProjects: "Engineering Projects"
+        },
+        //          about       
+        about: {
+            label: "01 / ABOUT ME",
+            titleStart: "Engineering with",
+            titleHighLight: "purpose.",
+            paragraph1: "I'm a Mechanical Design Engineer with more than seven years of experience in mechanical design, robotics and advanced product development.",
+            paragraph2: "My work combines CAD modeling, mechanical analysis, prototyping, manufacturing and testing to transform engineering concepts into reliable products.",
+            location: "Location",
+            locationValue: "Pisa, Italy",
+            education: "Education",
+            educationValue: "M.Sc. Mechanical Engineering",
+            specialization: "Specialization",
+            pecializationValue: "Applied Mechanics",
+            experience: "Experience",
+            experienceValue: "7+ Years",
+            downloadCV: "DOWNLOAD CV ↓"
+        },
+        //         EXPERTISE         
+        EXPERTISE: {
+            label: " 02 / EXPERTISE",
+            titleStart: "Technical",
+            titleHighLight: "capabilities.",
+            MechanicalDesign:{
+                title:"Mechanical Design",
+                description:"Mechanism design, component development and complete mechanical systems.",
+                tags:[
+                    "SolidWorks",
+                    "Inventor",
+                    "Creo"
+                ]
+            },
+            cad:{
+                title:"CAD & 3D Modeling",
+                description:"Advanced 3D modeling, assemblies,technical drawings and design documentation.",
+                tags:[
+                    "CAD",
+                    "GD&T",
+                    "Drafting"
+                ]
+            },
+            simulation:{
+                title:"Simulation & Analysis",
+                description:"Simulation-driven engineering and structural analysis for reliable designs.",
+                tags:[
+                    "FEA",
+                    "ABAQUS",
+                    "MATLAB"
+                ]
+            },
+            manufacturing:{
+                title:"Manufacturing",
+                description:"Design for manufacturing, prototyping,CNC machining and production documentation.",
+                tags:[
+                    "DFM",
+                    "CAM",
+                    "CNC"
+                ]
+            },
+            prototyping:{
+                title:"Prototyping",
+                description:"From first prototype to testing,validation and product improvement.",
+                tags:[
+                    "3D Printing",
+                    "Testing",
+                    "Assembly"
+                ]
+            },
+            projectLeadership:{
+                title:"Project Leadership",
+                description:"Leading multidisciplinary teams and managing engineering projects from conceptto production.",
+                tags:[
+                    "Leadership",
+                    "R&D",
+                    "Management"
+                ]
+            },
+        }
+    },
+    fa: {
+
+    }
+};
