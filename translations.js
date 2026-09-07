b34cf9bb7efd5d3be91b73068e87976eb231af4c
spd <spd.81.2002@gmail.com>
2026-09-07 04:21:04 +0330

    translation file add and fixed EN until education

--- translations.js
+++ translations.js
@@ -45,63 +45,148 @@ const translations = {
             label: " 02 / EXPERTISE",
             titleStart: "Technical",
             titleHighLight: "capabilities.",
-            MechanicalDesign:{
-                title:"Mechanical Design",
-                description:"Mechanism design, component development and complete mechanical systems.",
-                tags:[
+            MechanicalDesign: {
+                title: "Mechanical Design",
+                description: "Mechanism design, component development and complete mechanical systems.",
+                tags: [
                     "SolidWorks",
                     "Inventor",
                     "Creo"
                 ]
             },
-            cad:{
-                title:"CAD & 3D Modeling",
-                description:"Advanced 3D modeling, assemblies,technical drawings and design documentation.",
-                tags:[
+            cad: {
+                title: "CAD & 3D Modeling",
+                description: "Advanced 3D modeling, assemblies,technical drawings and design documentation.",
+                tags: [
                     "CAD",
                     "GD&T",
                     "Drafting"
                 ]
             },
-            simulation:{
-                title:"Simulation & Analysis",
-                description:"Simulation-driven engineering and structural analysis for reliable designs.",
-                tags:[
+            simulation: {
+                title: "Simulation & Analysis",
+                description: "Simulation-driven engineering and structural analysis for reliable designs.",
+                tags: [
                     "FEA",
                     "ABAQUS",
                     "MATLAB"
                 ]
             },
-            manufacturing:{
-                title:"Manufacturing",
-                description:"Design for manufacturing, prototyping,CNC machining and production documentation.",
-                tags:[
+            manufacturing: {
+                title: "Manufacturing",
+                description: "Design for manufacturing, prototyping,CNC machining and production documentation.",
+                tags: [
                     "DFM",
                     "CAM",
                     "CNC"
                 ]
             },
-            prototyping:{
-                title:"Prototyping",
-                description:"From first prototype to testing,validation and product improvement.",
-                tags:[
+            prototyping: {
+                title: "Prototyping",
+                description: "From first prototype to testing,validation and product improvement.",
+                tags: [
                     "3D Printing",
                     "Testing",
                     "Assembly"
                 ]
             },
-            projectLeadership:{
-                title:"Project Leadership",
-                description:"Leading multidisciplinary teams and managing engineering projects from conceptto production.",
-                tags:[
+            projectLeadership: {
+                title: "Project Leadership",
+                description: "Leading multidisciplinary teams and managing engineering projects from conceptto production.",
+                tags: [
                     "Leadership",
                     "R&D",
                     "Management"
                 ]
             },
+        },
+        //      experience
+        experience: {
+            label: "03 / EXPERIENCE",
+            titleStart: "My professional",
+            titleHighLight: "journey.",
+            researchFellow: {
+                date: "2022 — NOW",
+                title: " Research Fellow",
+                compony: "Institute of Mechanical Intelligence",
+                description: "Research and development in wearable robotics, soft exoskeletons, mechanism design and simulation-driven optimization.",
+            },
+            mechanicalDesignEngineer: {
+                date: "2022 — 2024",
+                title: "Mechanical Design Engineer",
+                compony: "Wearable Robotics",
+                description: " Designed robotic systems and mechanical components while working with CAD,simulation, prototyping and testing.",
+            },
+            designEngineer: {
+                date: "2021 — 2022",
+                title: "Design Engineer",
+                compony: "Baal Industries",
+                description: "Developed CAD models, BOMs, technical drawings and manufacturing documentation for mechanical structures."
+            },
+            founder: {
+                date: "2017 — 2021",
+                title: "Founder & Mechanical Engineer",
+                compony: " Borna Tech",
+                description: "Led product development from concept and prototyping to testing and assembly,including patented assistive robotic systems.",
+            },
+        },
+        //       PROJECTS
+        projects: {
+            label: "04 / SELECTED PROJECTS",
+            titleStart: "Engineering ideas",
+            titleHighLight: "built to work.",
+            description: "A selection of engineering projects showing the process from problem definition to design,analysis and final prototype.",
+            filters: {
+                all: "ALL",
+                automotive: "AUTOMOTIVE",
+                robotics: "ROBOTICS",
+                mechanicalDESIGN: "MECHANICAL DESIGN",
+                analyses: "ANALYSIS"
+            },
+            flexos: {
+                category: "ROBOTICS",
+                title: "FLEXOS",
+                description: "Wearable robotic system designed  to assist human movement through advanced mechanical mechanisms.",
+                tags: [
+                    "CAD",
+                    "Simulation",
+                    "Prototyping"
+                ],
+                button: "VIEW CASE STUDY →",
+            },
+            exotive: {
+                category: "MECHANICAL DESIGN",
+                title: "EXOTIVE",
+                description: "Advanced mechanical system developed through CAD modeling, mechanism design and prototype validation.",
+                tags: [
+                    "SolidWorks",
+                    "GD&T",
+                    "DFM"
+                ],
+                button: "VIEW CASE STUDY →",
+            },
+            automotive: {
+                category: "AUTOMOTIVE",
+                title: "VEHICLE SYSTEM",
+                description: "Mechanical system development for automotive applications with focus on performance and manufacturability.",
+                tags: [
+                    "CAD",
+                    "FEA",
+                    "Manufacturing"
+                ],
+                button: "VIEW CASE STUDY →",
+            },
+            engineeringanalysis: {
+                category: "ENGINEERING ANALYSIS",
+                title: "STRUCTURAL ANALYSIS",
+                description: "Simulation-based evaluation of mechanical  components to improve reliability and optimize the final design.",
+                tags: [
+                    "ABAQUS",
+                    "FEA",
+                    "MATLAB"
+                ],
+                button: "VIEW CASE STUDY →",
+            }
         }
     },
-    fa: {
-
-    }
 };
